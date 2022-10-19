95ac146daf6078c0a57f8aff7b8164ee8a9374a9
rishad paduvil <45rishadricu@gmail.com>
2022-10-19 12:36:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+// import packages
+import {Route,Router} from 'react-router-dom' 
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
